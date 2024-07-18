630c2a3a476ee6ffa3f36e969c36ca13413216f0
beejeeess <borstrze21@gmail.com>
2024-07-18 14:54:06 +0200

    added rom background

--- rtl/draw_bg.sv
+++ rtl/draw_bg.sv
@@ -1,76 +1,78 @@
-/**
- * Copyright (C) 2023  AGH University of Science and Technology
- * MTM UEC2
- * Author: Piotr Kaczmarczyk
- *
- * Description:
- * Draw background.
- */
-
-
 `timescale 1 ns / 1 ps
 
+//////////////////////////////////////////////////////////////////////////////
+/*
+ Module name:   draw_bg
+ Author:        Borys Strzebonski
+ */
+//////////////////////////////////////////////////////////////////////////////
+
+
 module draw_bg (
-    input  logic clk,
-    input  logic rst,
+        input  logic clk,
+        input  logic rst,
 
-    vga_if.out vga_bg_out,
-    vga_if.in vga_bg_in
-);
+        vga_if.out vga_bg_out,
+        vga_if.in vga_bg_in
+    );
 
-import vga_pkg::*;
+    import vga_pkg::*;
 
+// Parameters for ROM
+    localparam ADDR_WIDTH = 16;  // Adjust based on the size of the image
+    localparam DATA_WIDTH = 12;  // RGB pixel width
 
-/**
- * Local variables and signals
- */
+    /**
+     * Local variables and signals
+     */
+    logic [DATA_WIDTH-1:0] rgb_nxt;
+    logic [ADDR_WIDTH-1:0] pixel_addr;
+    logic [DATA_WIDTH-1:0] rom_data;
 
-logic [11:0] rgb_nxt;
+    /**
+     * ROM Instance
+     */
+    draw_bg_rom #(
+        .ADDR_WIDTH(ADDR_WIDTH),
+        .DATA_WIDTH(DATA_WIDTH)
+    ) u_draw_bg_rom (
+        .clk(clk),
+        .en(1'b1),  // Always enable the ROM
+        .addrA(pixel_addr),
+        .dout(rom_data)
+    );
 
-/**
- * Internal logic
- */
-
-always_ff @(posedge clk) begin : bg_ff_blk
-    if (rst) begin
-        vga_bg_out.vcount   <= '0;
-        vga_bg_out.vsync    <= '0;
-        vga_bg_out.vblnk    <= '0;
-        vga_bg_out.hcount   <= '0;
-        vga_bg_out.hsync    <= '0;
-        vga_bg_out.hblnk    <= '0;
-        vga_bg_out.rgb      <= '0;
-    end else begin
-        vga_bg_out.vcount <=  vga_bg_in.vcount;
-        vga_bg_out.vsync  <=  vga_bg_in.vsync;
-        vga_bg_out.vblnk  <=  vga_bg_in.vblnk;
-        vga_bg_out.hcount <=  vga_bg_in.hcount;
-        vga_bg_out.hsync  <=  vga_bg_in.hsync;
-        vga_bg_out.hblnk  <=  vga_bg_in.hblnk;
-        vga_bg_out.rgb    <= rgb_nxt;
+    /**
+     * Internal logic
+     */
+    always_ff @(posedge clk) begin : bg_ff_blk
+        if (rst) begin
+            vga_bg_out.vcount   <= '0;
+            vga_bg_out.vsync    <= '0;
+            vga_bg_out.vblnk    <= '0;
+            vga_bg_out.hcount   <= '0;
+            vga_bg_out.hsync    <= '0;
+            vga_bg_out.hblnk    <= '0;
+            vga_bg_out.rgb      <= '0;
+        end else begin
+            vga_bg_out.vcount <=  vga_bg_in.vcount;
+            vga_bg_out.vsync  <=  vga_bg_in.vsync;
+            vga_bg_out.vblnk  <=  vga_bg_in.vblnk;
+            vga_bg_out.hcount <=  vga_bg_in.hcount;
+            vga_bg_out.hsync  <=  vga_bg_in.hsync;
+            vga_bg_out.hblnk  <=  vga_bg_in.hblnk;
+            vga_bg_out.rgb    <= rgb_nxt;
+        end
     end
-end
 
-
-
-always_comb begin : bg_comb_blk
-    if ( vga_bg_in.vblnk ||  vga_bg_in.hblnk) begin             // Blanking region:
-        rgb_nxt = 12'h0_0_0;                    // - make it it black.
-    end else begin                              // Active region:
-        if ( vga_bg_in.vcount == 0)                     // - top edge:
-            rgb_nxt = 12'hf_f_0;                // - - make a yellow line.
-        else if ( vga_bg_in.vcount == VER_PIXELS)   // - bottom edge:
-            rgb_nxt = 12'hf_0_0;                // - - make a red line.
-        else if ( vga_bg_in.hcount == 0)                // - left edge:
-            rgb_nxt = 12'h0_f_0;                // - - make a green line.
-        else if ( vga_bg_in.hcount == HOR_PIXELS - 1)   // - right edge:
-            rgb_nxt = 12'h0_0_f;                // - - make a blue line.
-
-        // Add your code here.
-
-        else                                    // The rest of active display pixels:
-            rgb_nxt = 12'h8_8_8;                // - fill with gray. 
+    always_comb begin : bg_comb_blk
+        if (vga_bg_in.vblnk || vga_bg_in.hblnk) begin  // Blanking region:
+            rgb_nxt = 12'h0_0_0;  // - make it black.
+        end else begin  // Active region:
+            // Calculate the address for the current pixel
+            pixel_addr = vga_bg_in.vcount * HOR_PIXELS + vga_bg_in.hcount;
+            rgb_nxt = rom_data;  // Get the pixel data from ROM
+        end
     end
-end
 
 endmodule
--- rtl/image_rom_card.sv
+++ rtl/image_rom_card.sv
@@ -15,7 +15,7 @@
  **
  */
 //////////////////////////////////////////////////////////////////////////////
- module image_rom_card 
+module image_rom_card
 	#(parameter
 		ADDR_WIDTH = 13,
 		DATA_WIDTH = 12,
@@ -31,52 +31,32 @@
 	(* rom_style = "block" *) // block || distributed
 
 	logic [DATA_WIDTH-1:0] rom [2**ADDR_WIDTH-1:0]; // rom memory
-	
+
 	string data_path_serce;
 	string data_path_pik;
 	string data_path_romb;
 	string data_path_trefl;
 
 	always_ff @(posedge clk) begin : rom_read_blk
-			dout <= rom[addrA];
+		dout <= rom[addrA];
 	end
 
 
 	initial begin
 		if (CARD_SYMBOL == 0) begin
 			data_path_serce = $sformatf("../../rtl/card_data/%0d_serce.dat", CARD_NUMBER);
-    		$readmemh(data_path_serce, rom);
+			$readmemh(data_path_serce, rom);
 		end if (CARD_SYMBOL == 1) begin
 			data_path_pik = $sformatf("../../rtl/card_data/%0d_pik.dat", CARD_NUMBER);
-    		$readmemh(data_path_pik, rom);
+			$readmemh(data_path_pik, rom);
 		end if (CARD_SYMBOL == 2) begin
 			data_path_romb = $sformatf("../../rtl/card_data/%0d_romb.dat", CARD_NUMBER);
-    		$readmemh(data_path_romb, rom);
+			$readmemh(data_path_romb, rom);
 		end if (CARD_SYMBOL == 3) begin
 			data_path_trefl = $sformatf("../../rtl/card_data/%0d_trefl.dat", CARD_NUMBER);
-    		$readmemh(data_path_trefl, rom);
-		end 		
+			$readmemh(data_path_trefl, rom);
+		end
 	end
 
 endmodule
 
-<<<<<<< HEAD
-=======
-
-/**
- * Memory initialization from a file
- */
-
-/* Relative path from the simulation or synthesis working directory */
-initial $readmemh("../../rtl/card_data/AS_trefl.dat", rom);
-
-
-/**
- * Internal logic
- */
-
-always @(posedge clk)
-    rgb <= rom[address];
-
-endmodule
->>>>>>> 33ee601b071837783309a9b37af4d85df3d1ed26
